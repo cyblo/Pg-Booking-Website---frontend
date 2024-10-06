3ada6f4b2795bea147c95f33a694d897898a5849
Rachit Kala <152580018+cyblo@users.noreply.github.com>
2024-10-06 10:47:30 +0530

    Add files via upload

---
+++ about.css
@@ -0,0 +1,40 @@
+.container{
+    display: grid;
+    grid-template-columns: repeat(auto-fit,minmax(240px,auto));
+    gap: 5rem;
+    padding: 0 50px;
+}
+.container .box{
+    background-color: #ffffffda;
+    padding: 10px;
+    border-radius: 1rem;
+
+}
+.container .box:hover{
+    transform: translateY(-10px);
+    transition: 0.5s;
+}
+.container .box img{
+    width:100%;
+    border-radius: 1rem;
+    height: 400px;
+    object-fit: cover;
+    object-position: center;
+}
+.container .box h2{
+    font-family:sans-serif;
+    font-size:1.5rem;
+    width: 100%;
+    text-align: center;
+    color:#ff7200;
+}
+.container .box .content{
+    display: flex;
+    justify-content: space-between;
+    margin-top: 0.5rem;
+
+}
+.container .box .content button{
+    margin-left: 100px;
+    border-radius: 5px;
+}
